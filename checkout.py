a5716b9d48299a8ba3b905fb4b459347029e89c4
Andris Briedis <andris@printify.com>
2018-07-19 10:15:39 +0300

    Implementation from the first mob programming session

--- checkout.py
+++ checkout.py
@@ -1,14 +1,34 @@
 from pricing_rules import PricingRules
-from decimal import Decimal
 
-
+class MissingItem(Exception):
+    pass
 class CheckOut(object):
     def __init__(self, pricing_rules: PricingRules):
         self.pricing_rules = pricing_rules
+        self.items = []
+        self.prices = {
+            "A": {
+                "regular_price": 50,
+                "special_offer": {
+                    "count": 3,
+                    "price": 130
+                }
+            }
+        }
+
 
     def scan(self, sku: str) -> None:
-        pass
+        if sku not in self.prices:
+            raise MissingItem()
+        self.items.append(sku)
 
-    def total(self) -> Decimal:
-        return Decimal(0)
+    def total(self) -> float:
+        item_count = len(self.items)
+        total = 0
+        if not self.items:
+            return 0
+        special_offers_count = int(item_count / self.prices['A']['special_offer']['count'])
+        total = special_offers_count * self.prices['A']['special_offer']['price']
+        total += (item_count % self.prices['A']['special_offer']['count']) * self.prices['A']['regular_price']
 
+        return total
--- test.py
+++ test.py
@@ -1,12 +1,40 @@
 from pricing_rules import PricingRules
-from checkout import CheckOut
+from checkout import CheckOut, MissingItem
 from decimal import Decimal
 
 import unittest
 
 
 class CheckoutTest(unittest.TestCase):
+    def setUp(self):
+        self.pricing_rules = PricingRules()
+        self.checkout = CheckOut(self.pricing_rules)
+
     def test_sanity(self):
-        rules = PricingRules()
-        checkout = CheckOut(rules)
-        self.assertEqual(Decimal(0), checkout.total())
+        self.assertEqual(0, self.checkout.total())
+
+    def test_single_item(self):
+        self.checkout.scan("A")
+        self.assertEqual(50, self.checkout.total())
+
+    def test_non_existent_sku(self):
+        self.assertRaises(MissingItem, self.checkout.scan, "NON_EXISTING")
+
+    def test_double_item(self):
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.assertEqual(100, self.checkout.total())
+
+    def test_special_offer(self):
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.assertEqual(130, self.checkout.total())
+
+    def test_special_offer_and_additional_items(self):
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.checkout.scan("A")
+        self.assertEqual(230, self.checkout.total())
